ff8534ea7107a3c32aefe74c8d472e56db5fed27
anaralizada <anar_alizade@internet.ru>
2024-01-24 14:33:25 +0400

    tutorial 23

---
+++ src/main/java/hometask/Main.java
@@ -0,0 +1,47 @@
+package hometask;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        /*
+        Tutorial answers:
+        1.A
+        2.A
+        3.c
+        4.b
+        5.c
+        6.c
+        7.a
+        8.c
+        9.d
+        10.a
+        1.b
+        2.b
+        3.d
+        4.b
+        5.a
+        6.d
+        7.b
+        8.b
+        9.b
+        20.b
+
+        Round 2:
+        1.a
+        2.b
+        3.a
+        4.a
+        5.a
+        6.b
+        7.b
+        8.b
+        9.b
+        10.b
+        11.a
+        12.a
+        13.d
+        14.d
+         */
+    }
+}
